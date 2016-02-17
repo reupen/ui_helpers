e78b8662014c835315564ae361940c622b949027
Reupen Shah <reupen@users.noreply.github.com>
2016-02-17 21:32:24 +0000

    Modify ConfigIntegerDpiAware to allow migrations from old cfg_vars

--- config_var.h
+++ config_var.h
@@ -38,11 +38,11 @@ namespace uih {
 		TInteger value;
 		uint32_t dpi;
 
-		IntegerAndDpi(TInteger _value = NULL) : value(_value), dpi(USER_DEFAULT_SCREEN_DPI) {};
+		IntegerAndDpi(TInteger _value = NULL, uint32_t _dpi = USER_DEFAULT_SCREEN_DPI) : value(_value), dpi(_dpi) {};
 	};
 
 	template<typename TInteger>
-	class ConfigIntegerDpiAware {
+	class ConfigIntegerDpiAware : public cfg_var {
 	public:
 		using ValueType = IntegerAndDpi<TInteger>;
 		using Type = ConfigIntegerDpiAware<TInteger>;
@@ -52,22 +52,43 @@ namespace uih {
 		// https://msdn.microsoft.com/en-us/library/windows/desktop/dn280510%28v=vs.85%29.aspx
 		void set(TInteger value, uint32_t dpi = GetSystemDpiCached().cx)
 		{
-			m_Value.get_value().value = value;
-			m_Value.get_value().dpi = dpi;
+			m_Value.value = value;
+			m_Value.dpi = dpi;
+			on_change();
+		}
+		void set(IntegerAndDpi<TInteger> value)
+		{
+			m_Value = value;
 			on_change();
 		}
 		Type & operator = (TInteger value) { set(value);  return *this; }
+		Type & operator = (IntegerAndDpi<TInteger> value) { set(value);  return *this; }
 
 		operator TInteger () const { return getScaledValue(); }
 		const ValueType & getRawValue() const { return m_Value; };
 
-		TInteger getScaledValue() const { return MulDiv(m_Value.get_value().value, GetSystemDpiCached().cx, m_Value.get_value().dpi); };
+		TInteger getScaledValue() const { return MulDiv(m_Value.value, GetSystemDpiCached().cx, m_Value.dpi); };
 
 		virtual void on_change() {};
-		ConfigIntegerDpiAware(const GUID & guid, TInteger value) : m_Value(guid, ValueType(value))
+		ConfigIntegerDpiAware(const GUID & guid, TInteger value) : cfg_var(guid), m_Value(ValueType(value))
 		{};
+	protected:
+		void get_data_raw(stream_writer * p_stream, abort_callback & p_abort)
+		{
+			p_stream->write_lendian_t(m_Value.value, p_abort);
+			p_stream->write_lendian_t(m_Value.dpi, p_abort);
+		}
+		void set_data_raw(stream_reader * p_stream, t_size p_sizehint, abort_callback & p_abort)
+		{
+			p_stream->read_lendian_t(m_Value.value, p_abort);
+			// Allow migration from older config variables
+			if (p_sizehint > sizeof(TInteger))
+				p_stream->read_lendian_t(m_Value.dpi, p_abort);
+			else
+				m_Value.dpi = GetSystemDpiCached().cx; //If migrating from an older config var, assume it was set using the current system DPI.
+		}
 	private:
-		cfg_struct_t < IntegerAndDpi<TInteger> > m_Value;
+		IntegerAndDpi<TInteger> m_Value;
 	};
 
 	using ConfigUint32DpiAware = ConfigIntegerDpiAware<uint32_t>;
