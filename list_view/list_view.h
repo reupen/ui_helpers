reupen/ui_helpers

commit 041d0924c762c7b1d6f18585c9435886980d68cd

Preserve scroll position when reconfiguring list view controls

list_view/list_view.h
@@ -281,7 +281,8 @@ public:
     std::optional<IsVisibleResult> is_partially_visible(t_size index);
     bool is_fully_visible(t_size index);
     void ensure_visible(t_size index);
-    void scroll(int position, bool b_horizontal = false);
+
+    void scroll(int position, bool b_horizontal = false, bool update_display = true);
     void scroll_from_scroll_bar(short scroll_bar_command, bool b_horizontal = false);
 
     void get_item_group(t_size index, t_size level, t_size& index_start, t_size& count);
@@ -324,8 +325,8 @@ public:
     void reorder_items_partial(
         size_t base, const size_t* order, size_t count, bool update_focus_item = true, bool update_display = true);
 
-    t_size get_previous_item(int y, bool b_include_headers = false);
-    t_size get_next_item(int y, bool b_include_headers = false);
+    t_size get_previous_item(int y, bool b_include_headers = false) const;
+    t_size get_next_item(int y, bool b_include_headers = false) const;
     t_size get_last_viewable_item();
     t_size get_last_item();
     int get_default_item_height();
@@ -345,7 +346,7 @@ public:
 
     void clear_all_items() { m_items.remove_all(); }
 
-    int get_item_position(t_size index, bool b_include_headers = false)
+    int get_item_position(t_size index, bool b_include_headers = false) const
     {
         int ret = 0;
         if (index < m_items.get_count())
@@ -378,13 +379,7 @@ public:
         return ret;
     }
 
-    void refresh_item_positions(bool b_update_display = true)
-    {
-        __calculate_item_positions();
-        update_scroll_info();
-        if (b_update_display)
-            RedrawWindow(get_wnd(), nullptr, nullptr, RDW_INVALIDATE | RDW_UPDATENOW);
-    }
+    void refresh_item_positions(bool b_update_display = true);
 
     enum notification_source_t {
         notification_source_unknown,

list_view/list_view_hittest.cpp
@@ -151,30 +151,14 @@ t_size ListView::get_last_item()
     const auto item_area_height = get_item_area_height();
     return get_previous_item(m_scroll_position + item_area_height);
 }
-t_size ListView::get_previous_item(int y, bool b_include_headers)
+
+t_size ListView::get_previous_item(int y, bool b_include_headers) const
 {
-    {
-        t_size max = m_items.get_count();
-        t_size min = 0;
-        t_size ptr;
-        // if (max && y < m_items[0]->m_position)
-        //    return 0;
-        while (min < max) {
-            ptr = min + ((max - min) >> 1);
-            if (y > get_item_position(ptr, b_include_headers))
-                min = ptr + 1;
-            else if (y < get_item_position(ptr, b_include_headers))
-                max = ptr;
-            else {
-                return ptr;
-                // return true;
-            }
-        }
-        return min ? min - 1 : min;
-        // return true;
-    }
+    const auto next_item = get_next_item(y, b_include_headers);
+    return next_item > 0 ? next_item - 1 : next_item;
 }
-t_size ListView::get_next_item(int y, bool b_include_headers)
+
+t_size ListView::get_next_item(int y, bool b_include_headers) const
 {
     {
         t_size max = m_items.get_count();
@@ -193,7 +177,7 @@ t_size ListView::get_next_item(int y, bool b_include_headers)
                 // return true;
             }
         }
-        if (min == m_items.get_count())
+        if (min > 0 && min == m_items.get_count())
             min--;
         return min;
         // return true;

list_view/list_view_misc.cpp
@@ -2,6 +2,35 @@
 
 namespace uih {
 
+void ListView::refresh_item_positions(bool b_update_display)
+{
+    // Work out where the scroll position is proportionally between the first fully viewable item
+    // and the item before it
+    const auto previous_item_index = get_next_item(m_scroll_position);
+    const auto next_item_index = get_previous_item(m_scroll_position);
+    const auto next_item_top = get_item_position(next_item_index);
+    const auto previous_item_bottom = get_item_position(previous_item_index);
+    // If next_item_top == previous_item_bottom == 0, there are probably no items
+    const auto proportional_position = next_item_top != previous_item_bottom
+        ? static_cast<double>(m_scroll_position - previous_item_bottom)
+            / static_cast<double>(next_item_top - previous_item_bottom)
+        : 0.0;
+
+    __calculate_item_positions();
+    update_scroll_info();
+
+    // Restore the scroll position
+    const auto new_next_item_top = get_item_position(next_item_index);
+    const auto new_previous_item_bottom = get_item_position(previous_item_index);
+    const auto new_position = proportional_position * static_cast<double>(new_next_item_top - new_previous_item_bottom)
+        + new_previous_item_bottom;
+    const auto new_position_rounded = gsl::narrow<int>(std::lround(new_position));
+    scroll(new_position_rounded, false, false);
+
+    if (b_update_display)
+        RedrawWindow(get_wnd(), nullptr, nullptr, RDW_INVALIDATE | RDW_UPDATENOW);
+}
+
 bool ListView::copy_selected_items_as_text(t_size default_single_item_column)
 {
     pfc::string8 text, cleanedText;

list_view/list_view_scroll.cpp
@@ -27,7 +27,7 @@ void ListView::ensure_visible(t_size index)
     }
 }
 
-void ListView::scroll(int position, bool b_horizontal)
+void ListView::scroll(int position, bool b_horizontal, bool update_display)
 {
     const INT scroll_bar_type = b_horizontal ? SB_HORZ : SB_VERT;
     int& scroll_position = b_horizontal ? m_horizontal_scroll_position : m_scroll_position;
@@ -50,8 +50,10 @@ void ListView::scroll(int position, bool b_horizontal)
     int dy = 0;
     (b_horizontal ? dx : dy) = original_scroll_position - scroll_position;
 
-    ScrollWindowEx(get_wnd(), dx, dy, &playlist, &playlist, nullptr, nullptr, SW_INVALIDATE);
-    RedrawWindow(get_wnd(), nullptr, nullptr, RDW_UPDATENOW);
+    if (update_display) {
+        ScrollWindowEx(get_wnd(), dx, dy, &playlist, &playlist, nullptr, nullptr, SW_INVALIDATE);
+        RedrawWindow(get_wnd(), nullptr, nullptr, RDW_UPDATENOW);
+    }
 
     if (b_horizontal)
         reposition_header();

commit b84b52b9a1dea7130935489c03df417c667a0473

Fix inability to click on last items in list views
This fixes a regression in 041d092 which made it impossible to click on the last item in list views.

list_view/list_view.h
@@ -326,7 +326,7 @@ public:
         size_t base, const size_t* order, size_t count, bool update_focus_item = true, bool update_display = true);
 
     t_size get_previous_item(int y, bool b_include_headers = false) const;
-    t_size get_next_item(int y, bool b_include_headers = false) const;
+    t_size get_next_item(int y, bool b_include_headers = false, bool include_after_end = false) const;
     t_size get_last_viewable_item();
     t_size get_last_item();
     int get_default_item_height();

list_view/list_view_hittest.cpp
@@ -154,11 +154,11 @@ t_size ListView::get_last_item()
 
 t_size ListView::get_previous_item(int y, bool b_include_headers) const
 {
-    const auto next_item = get_next_item(y, b_include_headers);
+    const auto next_item = get_next_item(y, b_include_headers, true);
     return next_item > 0 ? next_item - 1 : next_item;
 }
 
-t_size ListView::get_next_item(int y, bool b_include_headers) const
+t_size ListView::get_next_item(int y, bool b_include_headers, bool include_after_end) const
 {
     {
         t_size max = m_items.get_count();
@@ -177,7 +177,7 @@ t_size ListView::get_next_item(int y, bool b_include_headers) const
                 // return true;
             }
         }
-        if (min > 0 && min == m_items.get_count())
+        if (!include_after_end && min > 0 && min == m_items.get_count())
             min--;
         return min;
         // return true;