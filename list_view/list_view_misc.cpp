reupen/ui_helpers

commit 8702284fadca243aaf9ee1ef67c96d7d41218978

Migrate list view to DirectWrite
This makes the list view use DirectWrite for text rendering rather than Uniscribe. (Text rendering in tooltips and inline edit controls remain unchanged.)

This also includes some fixes and improvements to positioning when rendering text using DirectWrite.

list_view/list_view_misc.cpp
@@ -269,7 +269,8 @@ void ListView::process_navigation_keydown(WPARAM wp, bool alt_down, bool repeat)
 
 int ListView::get_default_item_height()
 {
-    int ret = uih::get_font_height(m_items_font.get()) + m_vertical_item_padding;
+    const auto font_height = m_items_text_format ? m_items_text_format->get_minimum_height() : 0;
+    int ret = font_height + m_vertical_item_padding;
     if (ret < 1)
         ret = 1;
     return ret;
@@ -277,11 +278,13 @@ int ListView::get_default_item_height()
 
 int ListView::get_default_group_height()
 {
-    int ret = uih::get_font_height(m_group_font.get()) + m_vertical_item_padding;
+    const auto font_height = m_group_text_format ? m_items_text_format->get_minimum_height() : 0;
+    int ret = font_height + m_vertical_item_padding;
     if (ret < 1)
         ret = 1;
     return ret;
 }
+
 void ListView::on_focus_change(size_t index_prev, size_t index_new)
 {
     size_t count = m_items.size();
@@ -290,6 +293,7 @@ void ListView::on_focus_change(size_t index_prev, size_t index_new)
     if (index_new < count)
         invalidate_items(index_new, 1);
 }
+
 void ListView::invalidate_all(bool b_children, bool non_client)
 {
     auto flags = RDW_INVALIDATE | (b_children ? RDW_ALLCHILDREN : 0) | (non_client ? RDW_FRAME : 0);
@@ -601,41 +605,64 @@ void ListView::set_vertical_item_padding(int val)
     }
 }
 
-void ListView::set_font(const LPLOGFONT lplf)
+void ListView::set_font(const LOGFONT& log_font, std::optional<float> font_size)
 {
-    m_lf_items = *lplf;
-    m_lf_items_valid = true;
+    m_items_font_config = {log_font, font_size};
+
     if (m_initialised) {
         exit_inline_edit();
         destroy_tooltip();
-        m_items_font.reset(CreateFontIndirect(lplf));
-        m_item_height = get_default_item_height();
+
+        refresh_items_font();
+
         if (m_group_count)
             update_header();
+
         refresh_item_positions();
     }
 }
 
-void ListView::set_group_font(const LPLOGFONT lplf)
+void ListView::set_group_font(const LOGFONT& log_font, std::optional<float> font_size)
 {
-    m_lf_group_header = *lplf;
-    m_lf_group_header_valid = true;
+    m_group_font_config = {log_font, font_size};
+
     if (m_initialised) {
         exit_inline_edit();
         destroy_tooltip();
-        m_group_font.reset(CreateFontIndirect(lplf));
-        m_group_height = get_default_group_height();
+
+        refresh_group_font();
         refresh_item_positions();
     }
 }
 
-void ListView::set_header_font(const LPLOGFONT lplf)
+void ListView::refresh_items_font()
 {
-    m_lf_header = *lplf;
-    m_lf_header_valid = true;
+    if (m_direct_write_context && m_items_font_config)
+        m_items_text_format = m_direct_write_context->create_text_format_with_fallback(
+            m_items_font_config->log_font, m_items_font_config->size);
+
+    if (m_items_font_config)
+        m_items_font.reset(CreateFontIndirect(&m_items_font_config->log_font));
+
+    m_item_height = get_default_item_height();
+}
+
+void ListView::refresh_group_font()
+{
+    if (m_direct_write_context && m_group_font_config)
+        m_group_text_format = m_direct_write_context->create_text_format_with_fallback(
+            m_group_font_config->log_font, m_group_font_config->size);
+
+    m_group_height = get_default_group_height();
+}
+
+void ListView::set_header_font(const LOGFONT& log_font)
+{
+    m_header_log_font = log_font;
+
     if (m_initialised && m_wnd_header) {
         SendMessage(m_wnd_header, WM_SETFONT, NULL, MAKELPARAM(FALSE, 0));
-        m_header_font.reset(CreateFontIndirect(lplf));
+        m_header_font.reset(CreateFontIndirect(&log_font));
         SendMessage(m_wnd_header, WM_SETFONT, (WPARAM)m_header_font.get(), MAKELPARAM(TRUE, 0));
         on_size();
     }

direct_write.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 
+using namespace std::string_view_literals;
+
 namespace uih::direct_write {
 
 namespace {
@@ -228,16 +230,27 @@ void TextLayout::set_colour(COLORREF colour, DWRITE_TEXT_RANGE text_range) const
     THROW_IF_FAILED(m_text_layout->SetDrawingEffect(colour_effect.get(), text_range));
 }
 
-void TextLayout::render(HDC dc, RECT rect, float x, float y, COLORREF default_colour) const
+void TextLayout::render(HDC dc, RECT rect, COLORREF default_colour) const
 {
     const auto metrics = get_metrics();
 
-    const auto bitmap_width
-        = std::min(wil::rect_width(rect), gsl::narrow_cast<long>(metrics.width * m_scaling_factor + 1));
-    const auto bitmap_height = wil::rect_height(rect);
-    const auto source_x = rect.left + gsl::narrow_cast<int>(x)
-        + (bitmap_width < wil::rect_width(rect) ? gsl::narrow_cast<int>(metrics.left * m_scaling_factor) : 0);
-    const auto source_y = rect.top;
+    const auto draw_left_px = gsl::narrow_cast<long>(metrics.left * m_scaling_factor);
+    const auto draw_left_dip = gsl::narrow_cast<float>(draw_left_px) / m_scaling_factor;
+
+    const auto draw_right_px = gsl::narrow_cast<long>((metrics.left + metrics.width) * m_scaling_factor + 1);
+
+    const auto draw_top_px = gsl::narrow_cast<long>(metrics.top * m_scaling_factor);
+    const auto draw_top_dip = gsl::narrow_cast<float>(draw_top_px) / m_scaling_factor;
+
+    const auto draw_bottom_px = gsl::narrow_cast<long>((metrics.top + metrics.height) * m_scaling_factor + 1);
+
+    const auto rect_width = wil::rect_width(rect);
+    const auto rect_height = wil::rect_height(rect);
+
+    const auto bitmap_width = std::min(rect_width, draw_right_px - draw_left_px);
+    const auto bitmap_height = std::min(rect_height, draw_bottom_px - draw_top_px);
+    const auto source_x = rect.left + (bitmap_width < rect_width ? draw_left_px : 0l);
+    const auto source_y = rect.top + (bitmap_height < rect_height ? draw_top_px : 0);
 
     wil::com_ptr_t<IDWriteBitmapRenderTarget> bitmap_render_target;
     THROW_IF_FAILED(m_gdi_interop->CreateBitmapRenderTarget(dc, bitmap_width, bitmap_height, &bitmap_render_target));
@@ -253,7 +266,7 @@ void TextLayout::render(HDC dc, RECT rect, float x, float y, COLORREF default_co
 
     BitBlt(memory_dc, 0, 0, bitmap_width, bitmap_height, dc, source_x, source_y, SRCCOPY);
 
-    THROW_IF_FAILED(m_text_layout->Draw(NULL, renderer.get(), 0, y / m_scaling_factor));
+    THROW_IF_FAILED(m_text_layout->Draw(NULL, renderer.get(), -draw_left_dip, -draw_top_dip));
 
     BitBlt(dc, source_x, source_y, bitmap_width, bitmap_height, memory_dc, 0, 0, SRCCOPY);
 }
@@ -275,14 +288,31 @@ void TextFormat::disable_trimming_sign() const
     THROW_IF_FAILED(m_text_format->SetTrimming(&trimming, nullptr));
 }
 
+int TextFormat::get_minimum_height() const
+{
+    try {
+        auto max_size = 65536.0f;
+        const auto text_layout = create_text_layout(L""sv, max_size, max_size);
+        const auto metrics = text_layout.get_metrics();
+        auto top_half = max_size / 2 - metrics.top;
+        auto bottom_half = metrics.top + metrics.height - max_size / 2;
+
+        return gsl::narrow_cast<int>(
+            std::max(top_half, bottom_half) * 2.0f * TextLayout::s_default_scaling_factor() + 1);
+    }
+    CATCH_LOG()
+
+    return 1;
+}
+
 int TextFormat::measure_text_width(std::wstring_view text) const
 {
     try {
-        auto text_layout = create_text_layout(text, 65536.0f, 65536.0f);
-        auto metrics = text_layout.get_metrics();
+        const auto text_layout = create_text_layout(text, 65536.0f, 65536.0f);
+        const auto metrics = text_layout.get_metrics();
         return gsl::narrow_cast<int>((metrics.left + metrics.width) * TextLayout::s_default_scaling_factor() + 1);
     }
-    CATCH_LOG();
+    CATCH_LOG()
 
     return 0;
 }
@@ -304,7 +334,8 @@ TextLayout TextFormat::create_text_layout(std::wstring_view text, float max_widt
 
 Context::Context()
 {
-    THROW_IF_FAILED(DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), m_factory.put_unknown()));
+    THROW_IF_FAILED(
+        DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory1), m_factory.put_unknown()));
     THROW_IF_FAILED(m_factory->GetGdiInterop(&m_gdi_interop));
 }
 
@@ -337,21 +368,26 @@ TextFormat Context::create_text_format(const LOGFONT& log_font, float font_size)
     THROW_IF_FAILED(m_factory->CreateEllipsisTrimmingSign(text_format.get(), &trimming_sign));
 
     THROW_IF_FAILED(text_format->SetWordWrapping(DWRITE_WORD_WRAPPING_NO_WRAP));
+    THROW_IF_FAILED(text_format->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER));
 
     return {m_factory, m_gdi_interop, text_format, trimming_sign};
 }
 
-std::optional<TextFormat> Context::create_text_format_with_fallback(const LOGFONT& log_font, float font_size) noexcept
+std::optional<TextFormat> Context::create_text_format_with_fallback(
+    const LOGFONT& log_font, std::optional<float> font_size) noexcept
 {
+    const auto resolved_size = font_size.value_or(-log_font.lfHeight * gsl::narrow<float>(USER_DEFAULT_SCREEN_DPI)
+        / gsl::narrow<float>(get_system_dpi_cached().cx));
+
     try {
-        return create_text_format(log_font, font_size);
+        return create_text_format(log_font, resolved_size);
     }
     CATCH_LOG()
 
     LOGFONT icon_font{};
     if (SystemParametersInfo(SPI_GETICONTITLELOGFONT, 0, &icon_font, 0)) {
         try {
-            return create_text_format(icon_font, font_size);
+            return create_text_format(icon_font, resolved_size);
         }
         CATCH_LOG()
     }

direct_write.h
@@ -18,7 +18,7 @@ public:
     }
 
     DWRITE_TEXT_METRICS get_metrics() const;
-    void render(HDC dc, RECT rect, float x, float y, COLORREF default_colour) const;
+    void render(HDC dc, RECT rect, COLORREF default_colour) const;
     void set_colour(COLORREF colour, DWRITE_TEXT_RANGE text_range) const;
 
 private:
@@ -43,6 +43,7 @@ public:
     void enable_trimming_sign() const;
     void disable_trimming_sign() const;
 
+    [[nodiscard]] int get_minimum_height() const;
     [[nodiscard]] int measure_text_width(std::wstring_view text) const;
     [[nodiscard]] int measure_text_width(std::string_view text) const;
     [[nodiscard]] TextLayout create_text_layout(std::wstring_view text, float max_width, float max_height) const;
@@ -56,13 +57,30 @@ private:
 
 class Context {
 public:
+    using Ptr = std::shared_ptr<Context>;
+
+    static Ptr s_create()
+    {
+        auto ptr = s_ptr.lock();
+
+        if (!ptr) {
+            ptr = std::make_shared<Context>();
+            s_ptr = ptr;
+        }
+
+        return ptr;
+    }
+
     Context();
 
     TextFormat create_text_format(const LOGFONT& log_font, float font_size);
-    std::optional<TextFormat> create_text_format_with_fallback(const LOGFONT& log_font, float font_size) noexcept;
+    std::optional<TextFormat> create_text_format_with_fallback(
+        const LOGFONT& log_font, std::optional<float> font_size) noexcept;
 
 private:
-    wil::com_ptr_t<IDWriteFactory> m_factory;
+    inline static std::weak_ptr<Context> s_ptr;
+
+    wil::com_ptr_t<IDWriteFactory1> m_factory;
     wil::com_ptr_t<IDWriteGdiInterop> m_gdi_interop;
 };
 

direct_write_text_out.cpp
@@ -6,15 +6,15 @@ namespace uih::direct_write {
 
 namespace {
 
-int text_out_colours(const TextFormat& text_format, HDC dc, std::string_view text, int x_offset, const RECT& rect,
-    bool selected, DWORD default_color, alignment align, bool enable_colour_codes)
+int text_out_colours(const TextFormat& text_format, HDC dc, std::string_view text, const RECT& rect, bool selected,
+    DWORD default_color, alignment align, bool enable_colour_codes)
 {
     struct ColouredTextSegment {
         COLORREF colour{};
         size_t character_count{};
     };
 
-    if (is_rect_null_or_reversed(&rect) || rect.right <= rect.left + x_offset)
+    if (is_rect_null_or_reversed(&rect) || rect.right <= rect.left)
         return 0;
 
     const pfc::stringcvt::string_wide_from_utf8 utf16_text(text.data(), text.size());
@@ -94,10 +94,8 @@ int text_out_colours(const TextFormat& text_format, HDC dc, std::string_view tex
         }
 
         const auto metrics = layout.get_metrics();
-        const float y_offset
-            = (gsl::narrow_cast<float>(wil::rect_height(rect)) - metrics.height * scaling_factor) * 0.5f;
 
-        layout.render(dc, rect, gsl::narrow_cast<float>(x_offset), y_offset, default_color);
+        layout.render(dc, rect, default_color);
 
         return gsl::narrow_cast<int>(metrics.width * scaling_factor + 1);
     }
@@ -108,14 +106,14 @@ int text_out_colours(const TextFormat& text_format, HDC dc, std::string_view tex
 
 } // namespace
 
-void text_out_columns_and_colours(const TextFormat& text_format, HDC dc, std::string_view text, int x_offset,
-    int border, const RECT& rect, bool selected, COLORREF default_colour, bool enable_colour_codes,
-    bool enable_tab_columns, alignment align)
+int text_out_columns_and_colours(const TextFormat& text_format, HDC dc, std::string_view text, int x_offset, int border,
+    const RECT& rect, bool selected, COLORREF default_colour, bool enable_colour_codes, bool enable_tab_columns,
+    alignment align, bool enable_ellipses)
 {
     RECT adjusted_rect = rect;
 
     if (is_rect_null_or_reversed(&adjusted_rect))
-        return;
+        return 0;
 
     int tab_count = 0;
 
@@ -128,15 +126,19 @@ void text_out_columns_and_colours(const TextFormat& text_format, HDC dc, std::st
     }
 
     if (tab_count == 0) {
-        adjusted_rect.left += border;
+        adjusted_rect.left += border + x_offset;
         adjusted_rect.right -= border;
 
-        text_format.enable_trimming_sign();
-        text_out_colours(
-            text_format, dc, text, x_offset, adjusted_rect, selected, default_colour, align, enable_colour_codes);
-        return;
+        if (enable_ellipses)
+            text_format.enable_trimming_sign();
+        else
+            text_format.disable_trimming_sign();
+
+        return text_out_colours(
+            text_format, dc, text, adjusted_rect, selected, default_colour, align, enable_colour_codes);
     }
 
+    // Ellipses always disabled when using tab columns
     text_format.disable_trimming_sign();
 
     adjusted_rect.left += x_offset;
@@ -160,7 +162,7 @@ void text_out_columns_and_colours(const TextFormat& text_format, HDC dc, std::st
                 cell_rect.left = std::min(
                     adjusted_rect.right - MulDiv(cell_index, total_width, tab_count) + border, cell_rect.right);
 
-            const int cell_render_width = text_out_colours(text_format, dc, cell_text, 0, cell_rect, selected,
+            const int cell_render_width = text_out_colours(text_format, dc, cell_text, cell_rect, selected,
                 default_colour, cell_index == 0 ? ALIGN_RIGHT : ALIGN_LEFT, enable_colour_codes);
 
             if (cell_index == 0)
@@ -174,6 +176,8 @@ void text_out_columns_and_colours(const TextFormat& text_format, HDC dc, std::st
             cell_index++;
         }
     } while (position > 0);
+
+    return total_width;
 }
 
 } // namespace uih::direct_write

direct_write_text_out.h
@@ -2,8 +2,8 @@
 
 namespace uih::direct_write {
 
-void text_out_columns_and_colours(const TextFormat& text_format, HDC dc, std::string_view text, int x_offset,
-    int border, const RECT& rect, bool selected, COLORREF default_colour, bool enable_colour_codes,
-    bool enable_tab_columns, alignment align = uih::ALIGN_LEFT);
+int text_out_columns_and_colours(const TextFormat& text_format, HDC dc, std::string_view text, int x_offset, int border,
+    const RECT& rect, bool selected, COLORREF default_colour, bool enable_colour_codes, bool enable_tab_columns,
+    alignment align = uih::ALIGN_LEFT, bool enable_ellipses = true);
 
 } // namespace uih::direct_write

list_view/list_view.h
@@ -227,9 +227,9 @@ public:
         m_dark_edit_background_colour = background_colour;
     }
     void set_vertical_item_padding(int val);
-    void set_font(const LPLOGFONT lplf);
-    void set_group_font(const LPLOGFONT lplf);
-    void set_header_font(const LPLOGFONT lplf);
+    void set_font(const LOGFONT& log_font, std::optional<float> font_size = std::nullopt);
+    void set_group_font(const LOGFONT& log_font, std::optional<float> font_size = std::nullopt);
+    void set_header_font(const LOGFONT& log_font);
     void set_sorting_enabled(bool b_val);
     void set_show_sort_indicators(bool b_val);
     void set_edge_style(uint32_t b_val);
@@ -700,6 +700,11 @@ protected:
     void focus_search_box();
 
 private:
+    struct FontConfig {
+        LOGFONT log_font{};
+        std::optional<float> size;
+    };
+
     virtual void on_first_show();
 
     virtual void notify_on_focus_item_change(size_t new_index) {}
@@ -821,6 +826,9 @@ private:
     void reopen_themes();
     void close_themes();
 
+    void refresh_items_font();
+    void refresh_group_font();
+
     void create_header();
     void destroy_header();
     void set_header_window_theme() const;
@@ -831,11 +839,10 @@ private:
     void destroy_tooltip();
     void set_tooltip_window_theme() const;
     bool is_item_clipped(size_t index, size_t column);
-    int get_text_width(const char* text, size_t length);
+    int get_tooltip_text_width(const char* text, size_t length) const;
 
     wil::unique_hfont m_items_font;
     wil::unique_hfont m_header_font;
-    wil::unique_hfont m_group_font;
 
     bool m_use_dark_mode{};
     wil::unique_htheme m_list_view_theme;
@@ -862,12 +869,12 @@ private:
     COLORREF m_dark_edit_background_colour{};
     COLORREF m_dark_edit_text_colour{RGB(255, 255, 255)};
 
-    LOGFONT m_lf_items{0};
-    LOGFONT m_lf_header{0};
-    LOGFONT m_lf_group_header{0};
-    bool m_lf_items_valid{false};
-    bool m_lf_header_valid{false};
-    bool m_lf_group_header_valid{false};
+    std::optional<FontConfig> m_items_font_config{};
+    std::optional<FontConfig> m_group_font_config{};
+    std::optional<LOGFONT> m_header_log_font{};
+    direct_write::Context::Ptr m_direct_write_context;
+    std::optional<direct_write::TextFormat> m_items_text_format;
+    std::optional<direct_write::TextFormat> m_group_text_format;
 
     bool m_selecting{false};
     bool m_selecting_move{false};

list_view/list_view_drag_image.cpp
@@ -12,9 +12,9 @@ bool ListView::render_drag_image(LPSHDRAGIMAGE lpsdi)
 
     auto icon = get_drag_image_icon();
 
-    LOGFONT lf;
-    if (m_lf_items_valid)
-        lf = m_lf_items;
+    LOGFONT lf{};
+    if (m_items_font_config)
+        lf = m_items_font_config->log_font;
     else
         GetObject(m_items_font.get(), sizeof(lf), &lf);
 

list_view/list_view_header.cpp
@@ -17,7 +17,7 @@ void ListView::create_header()
     // if (m_show_header)
     {
         if (!m_wnd_header) {
-            m_header_font.reset(m_lf_header_valid ? CreateFontIndirect(&m_lf_header) : uih::create_icon_font());
+            m_header_font.reset(m_header_log_font ? CreateFontIndirect(&*m_header_log_font) : create_icon_font());
             m_wnd_header = CreateWindowEx(0, WC_HEADER, _T("NGLVH"),
                 WS_CHILD | (0) | /*(m_autosize ? 0x0800  : NULL) |*/ HDS_HOTTRACK
                     | (m_allow_header_rearrange ? HDS_DRAGDROP : NULL) | HDS_HORZ | HDS_FULLDRAG

commit e1f8ff2cbb82398e563686666daac35c1907380e

Correct list view group title height
This resolves a regression in 8702284fadca243aaf9ee1ef67c96d7d41218978 where list view group titles were using a height based on the items font, rather than the group font.

list_view/list_view_misc.cpp
@@ -278,7 +278,7 @@ int ListView::get_default_item_height()
 
 int ListView::get_default_group_height()
 {
-    const auto font_height = m_group_text_format ? m_items_text_format->get_minimum_height() : 0;
+    const auto font_height = m_group_text_format ? m_group_text_format->get_minimum_height() : 0;
     int ret = font_height + m_vertical_item_padding;
     if (ret < 1)
         ret = 1;